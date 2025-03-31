72d3914051fe9ce6e260e1df8cd15457469ac3e5
Wajiu Olarewaju Bello <129260539+Bellonee29@users.noreply.github.com>
2025-03-31 06:14:06 +0100

    adding necessary setup

--- auth-management/src/main/java/com/waystech/authmanagement/AuthManagementApplication.java
+++ auth-management/src/main/java/com/waystech/authmanagement/AuthManagementApplication.java
@@ -2,8 +2,10 @@ package com.waystech.authmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthManagementApplication {
 
 	public static void main(String[] args) {
